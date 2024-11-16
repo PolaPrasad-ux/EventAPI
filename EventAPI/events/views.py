3113756d4ce3a8e6e1598406c6b39778c6c79fea
Pola Prasad <pola@tcs.com>
2024-11-16 23:22:22 +0530

    cleaned views

--- EventAPI/events/views.py
+++ EventAPI/events/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from .models import User, Event, Ticket
-from .serializers import UserSerializer, EventSerializer, TicketSerializer
+from .serializers import EventSerializer
 from django.core.exceptions import ValidationError
 
 @api_view(['POST'])
